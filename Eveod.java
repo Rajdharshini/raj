08b97e11199de90a013757f08f7d5d9daebcb65a
Rajdharshini <rajvasanth307@gmail.com>
2017-06-06 03:27:13 -0700

    Create Eveod.java

---
+++ Eveod.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Eveod {
+	public static void main (String[] args) {
+	    Scanner s=new Scanner(System.in);
+		int x;
+		x=s.nextInt();
+		if(x%2==0)
+		{
+		    System.out.println("even");
+		}
+		else{
+		     System.out.println("odd");
+		}
+	}
+}
